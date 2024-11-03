43cec4d91180610640373ce7e6a1dd31dff5e8b2
seashellbrain <dashabasik@gmail.com>
2024-11-03 22:59:41 +0300

    version_5_winnn

---
+++ login_account.js
@@ -0,0 +1,27 @@
+var text1 = document.getElementById("text1");
+    		if(text1) {
+      			text1.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var text2 = document.getElementById("text2");
+    		if(text2) {
+      			text2.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var rectangle = document.getElementById("rectangle");
+    		if(rectangle) {
+      			rectangle.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
+    		
+    		var container = document.getElementById("container");
+    		if(container) {
+      			container.addEventListener("click", function (e) {
+        				// Add your code here
+      			});
+    		}
