8bb783868ce4973bf039424f06732648d4a61897
eneroff <komissarcikdanya@gmail.com>
2024-11-05 13:26:07 +0300

    version_5.2

---
+++ product_card.js
@@ -0,0 +1,55 @@
+var icons8512 = document.getElementById("icons8512");
+if(icons8512) {
+      icons8512.addEventListener("click", function (e) {
+            // Add your code here
+      });
+}
+
+var basket2Icon = document.getElementById("basket2Icon");
+if(basket2Icon) {
+      basket2Icon.addEventListener("click", function (e) {
+            // Add your code here
+      });
+}
+
+var text = document.getElementById("text");
+if(text) {
+      text.addEventListener("click", function (e) {
+            // Add your code here
+      });
+}
+
+var text1 = document.getElementById("text1");
+if(text1) {
+      text1.addEventListener("click", function (e) {
+            // Add your code here
+      });
+}
+
+var productButtonContainer = document.getElementById("productButtonContainer");
+if(productButtonContainer) {
+      productButtonContainer.addEventListener("click", function (e) {
+            // Add your code here
+      });
+}
+
+var text2 = document.getElementById("text2");
+if(text2) {
+      text2.addEventListener("click", function (e) {
+            // Add your code here
+      });
+}
+
+var text3 = document.getElementById("text3");
+if(text3) {
+      text3.addEventListener("click", function (e) {
+            // Add your code here
+      });
+}
+
+var text4 = document.getElementById("text4");
+if(text4) {
+      text4.addEventListener("click", function (e) {
+            // Add your code here
+      });
+}
