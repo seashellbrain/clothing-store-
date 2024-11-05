62385a19e896b236e0ae06fc8c8c9ede801c9e16
eneroff <komissarcikdanya@gmail.com>
2024-11-05 13:31:47 +0300

    version_6.1

---
+++ link_card.js
@@ -0,0 +1,6 @@
+var container = document.getElementById("container");
+if(container) {
+container.addEventListener("click", function (e) {
+// Add your code here
+});
+}
---
+++ registration.js
@@ -0,0 +1,18 @@
+var rectangle = document.getElementById("rectangle");
+if(rectangle) {
+rectangle.addEventListener("click", function (e) {
+// Add your code here
+});
+}
+var text3 = document.getElementById("text3");
+if(text3) {
+text3.addEventListener("click", function (e) {
+// Add your code here
+});
+}
+var container = document.getElementById("container");
+if(container) {
+container.addEventListener("click", function (e) {
+// Add your code here
+});
+}
