8bddbc6699566504a48e4ab78e0b417b4698e926
seashellbrain <dashabasik@gmail.com>
2024-11-12 19:22:21 +0300

    version_10

--- code/news/news.js
+++ code/news/news.js
@@ -1,48 +1,21 @@
-var icons8512 = document.getElementById("icons8512");
-    		if(icons8512) {
-      			icons8512.addEventListener("click", function (e) {
-        				// Add your code here
-      			});
-    		}
-    		
-    		var basket2Icon = document.getElementById("basket2Icon");
-    		if(basket2Icon) {
-      			basket2Icon.addEventListener("click", function (e) {
-        				// Add your code here
-      			});
-    		}
-    		
-    		var text = document.getElementById("text");
-    		if(text) {
-      			text.addEventListener("click", function (e) {
-        				// Add your code here
-      			});
-    		}
-    		
-    		var text1 = document.getElementById("text1");
-    		if(text1) {
-      			text1.addEventListener("click", function (e) {
-        				// Add your code here
-      			});
-    		}
-    		
-    		var text2 = document.getElementById("text2");
-    		if(text2) {
-      			text2.addEventListener("click", function (e) {
-        				// Add your code here
-      			});
-    		}
-    		
-    		var text3 = document.getElementById("text3");
-    		if(text3) {
-      			text3.addEventListener("click", function (e) {
-        				// Add your code here
-      			});
-    		}
-    		
-    		var text4 = document.getElementById("text4");
-    		if(text4) {
-      			text4.addEventListener("click", function (e) {
-        				// Add your code here
-      			});
-    		}
+//кнопка все категории
+
+function toggleCategories() {
+    const dropdown = document.getElementById("category-dropdown");
+    const button = document.querySelector(".category-button");
+
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        dropdown.style.display = "block";
+        dropdown.classList.remove("rounded-corners"); // Убираем верхние скругления
+        button.classList.add("button-opened"); // Добавляем класс для открытой кнопки
+        dropdown.classList.add("dropdown-opened"); // Добавляем класс для открытого меню
+        button.textContent = "Скрыть категории";
+    } else {
+        dropdown.style.display = "none";
+        button.classList.remove("button-opened"); // Убираем класс для закрытой кнопки
+        dropdown.classList.remove("dropdown-opened"); // Убираем класс для закрытого меню
+        button.textContent = "ВСЕ КАТЕГОРИИ";
+    }
+}
+
+
