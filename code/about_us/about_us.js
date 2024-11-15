9f97757ca19f4b1f79f1851b82b3e67aebe9e852
seashellbrain <dashabasik@gmail.com>
2024-11-15 13:35:48 +0300

    version_11

--- code/about_us/about_us.js
+++ code/about_us/about_us.js
@@ -46,3 +46,31 @@ function getUserStatus() {
 
 // Назначаем функцию на клик по кнопке "Личный кабинет"
 document.getElementById("text1").addEventListener("click", checkUserStatus);
+
+
+
+
+
+// Функция для анимации подсчета
+function animateCounter(id, start, end, duration) {
+    let range = end - start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / range));
+    let obj = document.getElementById(id);
+    let timer = setInterval(function() {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+// Вызов функции для каждого счетчика
+window.onload = function() {
+    animateCounter("counter1", 0, 100, 2000); // 100 тысяч доставок в год
+    animateCounter("counter2", 0, 20, 2000);  // 20 великих компаний
+    animateCounter("counter3", 0, 2, 2000);   // 2 тысячи сотрудников
+    animateCounter("counter4", 0, 10, 2000);  // 10 миллионов продукции
+};
--- code/return_obmen/return_obmen.js
+++ code/return_obmen/return_obmen.js
@@ -46,3 +46,45 @@ function getUserStatus() {
 
 // Назначаем функцию на клик по кнопке "Личный кабинет"
 document.getElementById("text1").addEventListener("click", checkUserStatus);
+
+
+
+//кнопка все категории
+
+function toggleCategories() {
+    const dropdown = document.getElementById("category-dropdown");
+    const button = document.querySelector(".category-button");
+
+    if (dropdown.style.display === "none" || dropdown.style.display === "") {
+        dropdown.style.display = "block";
+        dropdown.classList.remove("rounded-corners"); // Убираем верхние скругления
+        button.classList.add("button-opened"); // Добавляем класс для открытой кнопки
+        dropdown.classList.add("dropdown-opened"); // Добавляем класс для открытого меню
+        button.textContent = "Скрыть категории";
+    } else {
+        dropdown.style.display = "none";
+        button.classList.remove("button-opened"); // Убираем класс для закрытой кнопки
+        dropdown.classList.remove("dropdown-opened"); // Убираем класс для закрытого меню
+        button.textContent = "ВСЕ КАТЕГОРИИ";
+    }
+}
+
+document.getElementById('privacyPolicyLink').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    window.location.href = './../../code/personal_data/personal_data.html'; 
+});
+
+function toggleAnswer(element) {
+    const answer = element.nextElementSibling; // Находим блок ответа, который идет сразу после вопроса
+    const button = element.querySelector(".toggle-button"); // Находим кнопку в текущем вопросе
+  
+    if (answer.style.display === "block") {
+      // Если ответ виден, скрываем его и меняем текст кнопки на "+"
+      answer.style.display = "none";
+      button.textContent = "+"; 
+    } else {
+      // Если ответ скрыт, показываем его и меняем текст кнопки на "×"
+      answer.style.display = "block";
+      button.textContent = "×";
+    }
+  }
